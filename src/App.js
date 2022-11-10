a95fbce9b327c57137434b75c7f5bd518c703ad3
jebrong <92987206+jebrong@users.noreply.github.com>
2022-11-10 18:10:47 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+
+// const url = "https://course-api.com/react-tours-project";
+function App() {
+  return <h2>Tours Projechjt Setup</h2>;
+}
+
+export default App;
